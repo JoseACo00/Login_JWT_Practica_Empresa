363beeeb696b65d916e2a6b8ca9faa4b86b78aef
Josecoca02 <Josecoca0213@gmail.com>
2024-04-18 13:54:46 +0200

    Tocamos algo y no funciona el registro

--- 1_Back/src/Controller/LoginController.php
+++ 1_Back/src/Controller/LoginController.php
@@ -12,39 +12,7 @@
 
     class LoginController extends AbstractController
     {
-        private $jwtManager;
 
-        public function __construct(JWTTokenManagerInterface $jwtManager)
-        {
-            $this->jwtManager = $jwtManager;
-        }
-
-        #[Route('/login', name: 'app_login', methods: ['POST'])]
-        public function login(Request $request, AuthenticationUtils $authenticationUtils): JsonResponse
-        {
-            // Obtener el error de autenticación (si lo hay) y el último nombre de usuario intentado
-            $error = $authenticationUtils->getLastAuthenticationError();
-            $lastUsername = $authenticationUtils->getLastUsername();
-
-            // Verificar si hay un error de autenticación
-            if ($error) {
-                return new JsonResponse(['error' => 'Invalid credentials'], JsonResponse::HTTP_UNAUTHORIZED);
-            }
-
-            // Obtener el usuario autenticado
-            $user = $this->getUser();
-
-            // Verificar si el usuario existe
-            if (!$user) {
-                return new JsonResponse(['error' => 'User not found'], JsonResponse::HTTP_NOT_FOUND);
-            }
-
-            // Generar el token JWT utilizando el servicio JWTTokenManagerInterface
-            $token = $this->jwtManager->create($user);
-
-            // Devolver el token JWT en la respuesta
-            return new JsonResponse(['token' => $token]);
-        }
 
         /*#[Route('/getUser', name: 'getUser', methods: ['POST'])]
         public  function login(Request $request)
