e3bb88de8adb0c54a4e9b5caf39ee585f3cb77a4
Josecoca02 <Josecoca0213@gmail.com>
2024-04-23 19:13:44 +0200

    Remenber me funciona correctamente
    Si el usuario tiene marcado el checkbox se guardara en localstorage y por lo tanto si cierra el navegador o sale tendrá el token pero si no tiene marcado el checkbox se guardará en sessionStorage y se perderá el token  cuando cierre el navegador

--- Proyecto_Login/src/app/loggin/loggin.component.ts
+++ Proyecto_Login/src/app/loggin/loggin.component.ts
@@ -13,35 +13,37 @@ export class LogginComponent {
   mensaje="El campo es obligatorio"; // ESTO SIRVE PARA LA INTERPOLACIÓN
 
   constructor(private fb: FormBuilder, private LoginService:LoginService, private router: Router){
-    this.FormLogin = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl('')
-    });
+    // this.FormLogin = new FormGroup({
+    //   email: new FormControl(''),
+    //   password: new FormControl(''),
+    // });
   }
 
   FormLogin =this.fb.group({
     'email': ['', [Validators.required, Validators.email,  Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]], // Verifica el formato de correo electrónico
-    'password': ['', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]]
+    'password': ['', [Validators.required, Validators.minLength(6), Validators.maxLength(30)]],
+    'remenberMe' : [ false ]
   })
 
   procesar(){
     console.log(this.FormLogin.value);
   }
 
-  public LogOk(){
-    	this.LoginService.Login(this.FormLogin.value)
-      .subscribe(
-      (response) => {
-        console.log('Formulario Enviado:', response);
-        // Puedes agregar aquí lógica adicional después de enviar el formulario
-      },
-      (error) => {
-        console.error('Error al enviar el formulario:', error);
-        // Puedes manejar el error aquí, por ejemplo, mostrando un mensaje al usuario
-      },
 
-    ); this.router.navigate(['Inicio']);
-  }
+  // public LogOk(){
+  //   	this.LoginService.Login(this.FormLogin.value)
+  //     .subscribe(
+  //     (response) => {
+  //       console.log('Formulario Enviado:', response);
+  //       // Puedes agregar aquí lógica adicional después de enviar el formulario
+  //     },
+  //     (error) => {
+  //       console.error('Error al enviar el formulario:', error);
+  //       // Puedes manejar el error aquí, por ejemplo, mostrando un mensaje al usuario
+  //     },
+
+  //   ); this.router.navigate(['Inicio']);
+  // }
 
   // 1PONES EL TOKENJWT EN EL LOCALSTORAGE PERO NECESITAMOS PONER EL CONSTRUCTOR LAS VARIABLES
   onLogin() {
@@ -62,14 +64,44 @@ export class LogginComponent {
       );
   }
 
-  //2. CON FORMBUILDER COMO HICIMOS CON EL CREATE
+
+   //DEPENDIENDO DE SI ESTA MARCADO DEL CHECK BOX SE GUARDA EN LOCAL O SESSION STORAGE SISISSIISIS
+   public onLogin021() {
+    if (this.FormLogin.valid) {
+      const rememberMe = this.FormLogin.get('remenberMe')?.value;
+      this.LoginService.Login(this.FormLogin.value)
+        .subscribe(
+          (res: any) => {
+            if (res.token) {
+              if (rememberMe) {
+                localStorage.setItem('TokenJWT', res.token);
+              } else {
+                sessionStorage.setItem('TokenJWT', res.token);
+              }
+              alert('Inicio de sesión exitoso');
+              this.router.navigate(['/Inicio']); // Redirige a la página principal después del inicio de sesión
+            } else {
+              alert('Credenciales inválidas');
+            }
+          },
+          (error: { message: string; }) => {
+            alert('Error en la solicitud: ' + error.message);
+          }
+        );
+    } else {
+      alert('Por favor, completa todos los campos del formulario.');
+    }
+  }
+
+
+  //2. CON FORMBUILDER COMO HICIMOS CON EL CREATE SE GUARDA EN SESSIONSOTREAGE
   public onLogin2() {
     if (this.FormLogin.valid) {
       this.LoginService.Login(this.FormLogin.value)
         .subscribe(
           (res: any) => {
             if (res.token) {
-              localStorage.setItem('TokenJWT', res.token);
+              sessionStorage.setItem('TokenJWT', res.token);
               alert('Inicio de sesión exitoso');
               this.router.navigate(['/Inicio']); // Redirige a la página principal después del inicio de sesión
             } else {
@@ -84,6 +116,37 @@ export class LogginComponent {
       alert('Por favor, completa todos los campos del formulario.');
     }
   }
+
+  //SE GUARDA EN LOCALSTORAGE o SESION STORAGE DEPENDE DEL CHECK BOX QUE TIENE UN  formControlName="remenberMe"
+  //FALLO TE LO GUARDA EN LOS 2 PUEDE SER UTIL O NO EL TIEMPO LO DIRÁ
+  public onLogin02() {
+    if (this.FormLogin.valid) {
+      this.LoginService.Login(this.FormLogin.value)
+        .subscribe(
+          (res: any) => {
+            if(this.FormLogin.get('remenberMe')?.value) {
+              localStorage.setItem('TokenJWT', res.token);
+            }
+            if (res.token) {
+              sessionStorage.setItem('TokenJWT', res.token);
+              alert('Inicio de sesión exitoso');
+              this.router.navigate(['/Inicio']); // Redirige a la página principal después del inicio de sesión
+            } else {
+              alert('Credenciales inválidas');
+            }
+          },
+          (          error: { message: string; }) => {
+            alert('Error en la solicitud: ' + error.message);
+          }
+        );
+    } else {
+      alert('Por favor, completa todos los campos del formulario.');
+    }
+  }
+
+
+
+
 
 
 }
