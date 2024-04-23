4e214b43f4b27c4541cb8bc2370926f321304360
Josecoca02 <Josecoca0213@gmail.com>
2024-04-24 01:28:01 +0200

    Checkbox remenber me funciona
    Parece que todo va bien dependiendo de si hace click en el check box

--- Proyecto_Login/src/app/guards/login-guard.guard.ts
+++ Proyecto_Login/src/app/guards/login-guard.guard.ts
@@ -15,7 +15,7 @@ import { Observable } from 'rxjs';
 
 export const LogginGuard = () => {
     const router = inject(Router)
-    if (localStorage.getItem('TokenJWT')){
+    if (localStorage.getItem('TokenJWT')|| sessionStorage.getItem('TokenJWT')){
       return true;
     } else {
         // RETORMANOS A LA PAGINA QUE QUERAMOS
